70ccf36707f9535a09ebada737a3ca383f6ba9be
Gautam <mrg.gautam@gmail.com>
2026-07-30 00:05:59 +0530

    Add: matchsticks-to-square - submission-3

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-3.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean makesquare(int[] matchsticks) {
+        int n = matchsticks.length;
+        int sum = Arrays.stream(matchsticks).sum();
+        if(sum%4 != 0) {
+            return false;
+        }
+        return dfs(matchsticks, new boolean[n], sum/4, 4, 0, 0);
+    }
+
+
+    private boolean dfs(int[] nums, boolean[] used, int target,  int k, int currentSum, int start) {
+        if(k == 0) {
+            return true;
+        }
+        if(currentSum == target) {
+            return dfs(nums, used, target, k-1, 0, 0);
+        }
+
+        for(int i=start; i< nums.length; i++) {
+            if(used[i] || currentSum+nums[i] > target) {
+                continue;
+            }
+            used[i] = true;
+            if(dfs(nums, used, target, k, currentSum+nums[i], i+1)) {
+                return true;
+            }
+            used[i] = false;
+        }
+        return false;
+    }
+}
